4dd8ea24f434b2ff9242772b2c8e16352f5c9290
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-10 11:07:16 +0530

    Minor changes

--- app/src/main/java/com/edocent/easyplayer/MainFragment.java
+++ app/src/main/java/com/edocent/easyplayer/MainFragment.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +14,8 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
+    Button playButton;
+    Button stopButton;
 
     public MainFragment() {
         // Required empty public constructor
@@ -22,8 +25,12 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        playButton = (Button) view.findViewById(R.id.playButtonId);
+        stopButton = (Button) view.findViewById(R.id.stopButtonId);
+
+        return view;
     }
 
 
